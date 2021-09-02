6e5448df27aa1c18f355cfcf612f94064fb3f1d4
afeilke1 <afeilke1@hs-mittweida.de>
2021-09-02 21:09:27 +0200

    activate platform on contact

--- Assets/Scripts/PlatformMover.cs
+++ Assets/Scripts/PlatformMover.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlatformMover : MonoBehaviour
 {
+    public float speed = 1;
+    public bool startOnContact = true;
     public Transform[] points;
-    public float speed = 1, dist;
+
     private int pnum = 0, pnext = 1, pdir = 1;
     private Rigidbody2D rb;
+    private bool enabled = true;
 
     private void Awake()
     {
         if (points.Length == 0) Destroy(this);
+
         rb = GetComponent<Rigidbody2D>();
+        enabled = !startOnContact;
+
         Debug.Log("P " + name + " " + pnum);
     }
 
@@ -22,19 +28,23 @@ public class PlatformMover : MonoBehaviour
         pnum += pdir;
         if (pnum == 0 || pnum == points.Length - 1) pdir = -pdir;
         pnext = pnum + pdir;
-        Debug.Log("P " + name + " " + pnum + ":" + pnext);
+        // Debug.Log("P " + name + " " + pnum + ":" + pnext);
     }
 
     void FixedUpdate()
     {
         // skip if no checkpoints
-        if (points.Length <= 1) return;
+        if (!enabled || points.Length <= 1) return;
 
         Vector2 dir = (points[pnext].position - points[pnum].position).normalized;
         rb.MovePosition(rb.position + dir * speed * Time.fixedDeltaTime);
 
-        dist = Vector3.Distance(rb.position, points[pnum].position);
         if (Vector3.Distance(rb.position, points[pnext].position) < 2 * speed * Time.fixedDeltaTime)
             NextCheckpoint();
+    }
+
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+        if (!enabled && c.gameObject.CompareTag("Player")) enabled = true;
     }
 }
