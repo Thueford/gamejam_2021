eecaeb8e9b430a44d781af4d30126070166beb71
lwutke <lwutke@hs-mittweida.de>
2021-09-02 21:00:35 +0200

    the panel georg wanted

---
+++ Assets/Scripts/InvertGravityScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvertGravityScript : MonoBehaviour
+{
+    public Image panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //invert gravity here
+    public void btnPressed_InvertGravityButton()
+    {
+
+    }
+}
