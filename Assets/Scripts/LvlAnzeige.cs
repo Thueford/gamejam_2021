a7dc171844ebe74c2129a40af8506841f28bdde5
lwutke <lwutke@hs-mittweida.de>
2021-09-03 11:41:02 +0200

    destroyed something, hope it works at some point in time

---
+++ Assets/Scripts/LvlAnzeige.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlAnzeige : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI tmpro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmpro.text = StageManager.curStage.index;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tmpro.text = StageManager.curStage.index;
+    }
+}
