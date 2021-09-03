a8aca412b8043b4557b0eecbdc12e645f14e7054
afeilke1 <afeilke1@hs-mittweida.de>
2021-09-03 12:59:34 +0200

    added cam controller

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    [Range(1,50)]
+    public float lazyness;
+    public GameObject target;
+
+    private void LateUpdate()
+    {
+        if (!target) return;
+        Vector2 dir = target.transform.position - transform.position;
+        transform.position += (Vector3)dir / lazyness;
+    }
+}
