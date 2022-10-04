ca17086a9a3a0309ffced485b0d3dbd87424af27
MysticBanana <89936555+MysticBanana@users.noreply.github.com>
2022-10-04 11:37:40 +0200

    newlevel

--- Assets/Scripts/Controller/LvlStarter.cs
+++ Assets/Scripts/Controller/LvlStarter.cs
@@ -7,7 +7,7 @@ public class LvlStarter : MonoBehaviour
     void Start()
     {
         try { StageManager.LoadStage(PlayerPrefs.GetInt("choosenlvl")); }
-        catch { StageManager.LoadStage(5); }
+        catch { StageManager.LoadStage(1); }
 
         // todo remove
         // StageManager.LoadStage(3);
